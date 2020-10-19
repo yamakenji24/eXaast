da8db65df2aef70b6b4bbefb25b03c315c836adf
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-10-20 04:11:59 +0900

    entry files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './ui/test';
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase'
+import { Header } from './ui/Header';
+import TopPageContainer from './ui/toppage/TopPageContainer';
+import {TestContainer} from './ui/testpage/TestContainer';
+import { rootStore, rrfProps } from './stores/root-stores';
+import './index.scss';
 
-const App = () => {
-  return <Test />
-}
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Router>
+      <Provider store={rootStore}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={TopPageContainer} />
+            <Route path="/test" component={TestContainer} />
+          </Switch>
+        </ReactReduxFirebaseProvider>
+      </Provider>
+    </Router>,
+    document.getElementById('root')
+  );
+})
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
